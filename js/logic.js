676ffc1364b28a601867ca44db265de105d92be9
anumala thapa <anuthapamagar89@gmail.com>
2020-12-16 16:57:35 -0500

    script modified to adjust the radius of the marker

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,7 @@
 // Creating map object
 var myMap = L.map("mapid", {
     center: [37.09, -95.71],
-    zoom: 5
+    zoom: 3
     //layers: [streetmap, earthquakes]
 });
   
@@ -19,58 +19,69 @@ var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/signif
 // variable to keep geojson layer
 var geojson;
 
-// function to change color according to depth
-function Color(d) {
-  if (d > 90) {
-    return 'red'
-  } 
-  else if (d > 70) {
-    return 'orange'
-  } 
-  else if (d > 50) {
-    return "yellow"
-  } 
-  else if (d > 30) {
-    return 'lightgreen'
-  } 
-  else if (d > 10) {
-    return 'limegreen'
-  } 
-  else {
-    return 'green'
+// functions to style the circle marker
+function styleInfo(feature) {
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: getColor(feature.geometry.coordinates[2]),
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+} 
+
+// function to get the correct radius of the circle marker
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
   }
-};
+  return magnitude * 4;
+}
+
+// function to change color according to depth
+function getColor(depth) {
+  switch (true) {
+  case depth > 90:
+    return "#EA2C2C";
+  case depth > 70:
+    return "#EA822C";
+  case depth > 50:
+    return "#EE9C00";
+  case depth > 30:
+    return "#EECC00";
+  case depth > 10:
+    return "#D4EE00";
+  default:
+    return "#98EE00";
+  }
+}
 
 // grab data with d3
 d3.json(queryURL, function(data) {
   console.log(data)
-
-  // function to style the circle
-  function style(feature) {
-    return {
-      radius: feature.properties.mag *4,
-      fillOpacity: 0.75,
-      color: "black",
-      fillColor: Color(feature.geometry.coordinates[2]),
-      weight: 1,
-      opacity: 0
-    };
-  } 
-
+  
   //Create a GeoJSON layer containing the features
-  geojson = L.geoJson(data.features, {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-    pointTolayer: function (feature, latlng) {
-      return L.circleMarker(latlng, style(feature));
+  geojson = L.geoJson(data, {
+    // turn each feature into a circleMarker on the map
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
     },
-    onEachFeature : function(feature, layer) {
-    console.log(feature)
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    // set the style for each circleMarker using our styleInfo function
+    style: styleInfo,
+    // create a popup for each marker 
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup(
+        "Magnitude: "
+          + feature.properties.mag
+          + "<br>Depth: "
+          + feature.geometry.coordinates[2]
+          + "<br>Location: "
+          + feature.properties.place
+      );
     }
-  }).addTo(myMap)     
+  }).addTo(myMap);
 
   // Set up the legend
   var legend = L.control({ position: "bottomright" });
@@ -78,10 +89,10 @@ d3.json(queryURL, function(data) {
     
     var div = L.DomUtil.create("div", "info legend");
     var depth = [0, 10, 30, 50, 70, 90];
- 
+    
     for (var i = 0; i < depth.length; i++) {
       div.innerHTML +=
-        '<i style = "background:'+ Color(depth[i] +1) +'"></i> ' +
+        '<i style = "background:'+ getColor(depth[i] +1) +'"></i> ' +
         depth[i] + (depth[i + 1] ? '&ndash;'+ depth[i +1] +'<br>' :'+');
     }
     return div;
